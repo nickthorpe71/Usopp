8f14a8d31aa359186f51825e1ed266e4a457c86a
Nick Thorpe <nickthorpe71@gmail.com>
2022-10-02 12:43:40 -0500

    add change data into pytorch dataset

--- train.py
+++ train.py
@@ -2,6 +2,8 @@ import json
 from nltk_utils import bag_of_words, tokenize, stem
 import numpy as np
 
+from torch.utils.data import Dataset, DataLoader
+
 with open('intents.json', 'r') as f:
   intents = json.load(f)
   
@@ -35,3 +37,22 @@ for (pattern_sentence, tag) in xy:
   
 X_train = np.array(X_train)
 y_train = np.array(y_train)
+
+class ChatDataset(Dataset):
+  def __init__(self):
+    self.num_samples = len(X_train)
+    self.x_data = X_train
+    self.y_data = y_train
+    
+  def __getitem__(self, index):
+    return self.x_data[index], self.y_data[index]
+  
+  def __len__(self):
+    return self.num_samples
+ 
+# Hyper Params
+batch_size = 8
+  
+dataset = ChatDataset()
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
