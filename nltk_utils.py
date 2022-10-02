4a3031d4e927b7dfd871060b4a7cbaf7f7c3755e
Nick Thorpe <nickthorpe71@gmail.com>
2022-10-02 12:37:27 -0500

    add requirements, add data cleaning and normalizing

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,4 +1,5 @@
 import nltk
+import numpy as np
 # nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 
@@ -11,4 +12,10 @@ def stem(word):
   return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-  pass
+  tokenized_sentence = [stem(w) for w in tokenized_sentence]
+  bag =  np.zeros(len(all_words), dtype=np.float32)
+  for idx, w, in enumerate(all_words):
+    if w in tokenized_sentence:
+      bag[idx] = 1
+    
+  return bag
