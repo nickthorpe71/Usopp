ad9e8177f27dbaae9fbd5c99c0788c991dac365d
Nick Thorpe <nickthorpe71@gmail.com>
2022-10-09 15:44:35 -0500

    clean script data

--- v2/parse_script.py
+++ v2/parse_script.py
@@ -1,3 +1,4 @@
+import pandas as pd
 
 data = {
   'name': [],
@@ -6,4 +7,16 @@ data = {
 
 with open('data/chrono_cross_game_script.txt', 'rt') as script_file:
   for line in script_file.readlines():
-    print(f"|{line}|")
+    if len(line.strip()) == 0: 
+      continue
+    elif ':' in line:
+      data['name'].append(line.replace(":", "").strip())
+      data['dialog'].append("")
+    else:
+      current_dialog_index = len(data['dialog']) - 1
+      current_dialog = data['dialog'][current_dialog_index]
+      data['dialog'][current_dialog_index] = current_dialog + " " + line.strip()
+
+df = pd.DataFrame(data)
+print(df.head())
+print(df.tail())
