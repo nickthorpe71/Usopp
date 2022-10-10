fb89fd15b6fa2129759afe6f726fbe7875eda1bc
Nick Thorpe <nickthorpe71@gmail.com>
2022-10-09 19:45:16 -0500

    add raw data parser and cleaned csv

--- v2/parse_script.py
+++ v2/parse_script.py
@@ -15,8 +15,25 @@ with open('data/chrono_cross_game_script.txt', 'rt') as script_file:
     else:
       current_dialog_index = len(data['dialog']) - 1
       current_dialog = data['dialog'][current_dialog_index]
-      data['dialog'][current_dialog_index] = current_dialog + " " + line.strip()
+      data['dialog'][current_dialog_index] = f"{current_dialog} {line.strip()}"
 
 df = pd.DataFrame(data)
-print(df.head())
-print(df.tail())
+
+# create list of all characters
+all_characters = set(data['name'])
+
+# track all characters with more than 30 lines
+characters_with_lines = []
+for name in all_characters:
+  total_lines = sum(df['name'] == name)
+  if total_lines > 50:
+    characters_with_lines.append(name)
+
+# filter out any characters that have < 30 lines
+df = df[df['name'].isin(characters_with_lines)]
+
+
+df.to_csv('data/formatted_script.csv', index=False)
+
+
+
