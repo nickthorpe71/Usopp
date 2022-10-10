a49eac850e098d862d2769c5340e87836c5870b5
Nick Thorpe <nickthorpe71@gmail.com>
2022-10-09 21:58:37 -0500

    fix file imports for v1

--- v1/chatbot.py
+++ v1/chatbot.py
@@ -7,10 +7,10 @@ from nltk_utils import bag_of_words, tokenize
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Load model data, trained model, and intents data
-with open('intents.json', 'r') as f:
-  intents = json.load(f)
-  
-FILE = "data.pth"
+with open('v1/intents.json', 'r') as f:
+    intents = json.load(f)
+
+FILE = "v1/data.pth"
 data = torch.load(FILE)
 
 input_size = data.get("input_size")
@@ -29,25 +29,25 @@ bot_name = "Usopp"
 print("---USOPP CHATBOT---\nType 'quit' to end conversation.")
 
 while True:
-  sentence = input('You: ')
-  if sentence == "quit":
-    break
-  
-  sentence = tokenize(sentence)
-  X = bag_of_words(sentence, all_words)
-  X = X.reshape(1, X.shape[0])
-  X = torch.from_numpy(X)
-  
-  output = model(X)
-  _, predicted = torch.max(output, dim=1)
-  tag = tags[predicted.item()]
-  
-  probabilities = torch.softmax(output, dim=1)
-  probability = probabilities[0][predicted.item()]
-  
-  if probability.item() > 0.75: 
-    for intent in intents["intents"]:
-      if tag == intent["intent"]:
-        print(f"{bot_name}: {random.choice(intent['responses'])}")
-  else:
-    print(f"{bot_name}: What do you think?")
+    sentence = input('You: ')
+    if sentence == "quit":
+        break
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+
+    probabilities = torch.softmax(output, dim=1)
+    probability = probabilities[0][predicted.item()]
+
+    if probability.item() > 0.75:
+        for intent in intents["intents"]:
+            if tag == intent["intent"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    else:
+        print(f"{bot_name}: What do you think?")
