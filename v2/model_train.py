1ac14c5c743ba6798cd4a66fff3dc308c7a834e7
Nick Thorpe <nickthorpe71@gmail.com>
2022-10-09 20:43:38 -0500

    add apex package

--- v2/model_train.py
+++ v2/model_train.py
@@ -1,1 +1,223 @@
 # Train a GPT (Generative Pre-Trained Transformer)
+import glob
+import logging
+import os
+import pickle
+import random
+import re
+import shutil
+from typing import List
+
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+import torch
+# from torch.nn.utils.rnn import pad_sequence
+# from torch.utils.data import DataLoader, Dataset, RandomSampler, SequentialSampler
+# from torch.utils.data.distributed import DistributedSampler
+
+# from pathlib import Path
+
+from transformers import (
+    MODEL_WITH_LM_HEAD_MAPPING,
+    # WEIGHTS_NAME,
+    # AdamW,
+    # AutoConfig,
+    # PreTrainedModel,
+    AutoModelWithLMHead, AutoTokenizer,
+    PreTrainedTokenizer,
+    # get_linear_schedule_with_warmup
+)
+
+# try:
+#   from torch.utils.tensorboard import SummaryWriter
+# except ImportError:
+#   from tensorboardX import SummaryWriter
+
+data = pd.read_csv('data/formatted_script.csv')
+
+CHARACTER_NAME = 'Kid'
+
+contexted = []
+
+# context window of size 7
+n = 7
+
+for i in data[data.name == CHARACTER_NAME].index:
+    if i < n:
+        continue
+    row = []
+    prev = i - 1 - n  # we additionally subtract 1 so the row wil contain current response and 7 previous ones
+    for j in range(i, prev, -1):
+        row.append(data.dialog[j])
+    contexted.append(row)
+
+columns = ['response', 'context']
+columns = columns + ['context/' + str(i) for i in range(1, n)]
+
+df = pd.DataFrame.from_records(contexted, columns=columns)
+
+trn_df, val_df = train_test_split(df, test_size=0.1)
+
+# create dataset suitable for model
+
+
+def construct_conv(row, tokenizer, eos=True):
+    def flatten(l): return [item for sublist in l for item in sublist]
+    conv = list(
+        reversed([tokenizer.encode(x) + [tokenizer.eos_token_id] for x in row]))
+    conv = flatten(conv)
+    return conv
+
+
+class ConversationDataset(Dataset):
+    def __init__(self, tokenizer: PreTrainedTokenizer, args, df, block_size=512):
+
+        block_size = block_size - \
+            (tokenizer.model_max_length - tokenizer.max_len_single_sentence)
+
+        directory = args.cache_dir
+        cached_features_file = os.path.join(
+            directory, args.model_type + "_cached_lm_" + str(block_size)
+        )
+
+        if os.path.exists(cached_features_file) and not args.overwrite_cache:
+            logger.info("Loading features from cached file %s",
+                        cached_features_file)
+            with open(cached_features_file, "rb") as handle:
+                self.examples = pickle.load(handle)
+        else:
+            logger.info(
+                "Creating features from dataset file at %s", directory)
+
+            self.examples = []
+            for _, row in df.iterrows():
+                conv = construct_conv(row, tokenizer)
+                self.examples.append(conv)
+
+            logger.info("Saving features into cached file %s",
+                        cached_features_file)
+            with open(cached_features_file, "wb") as handle:
+                pickle.dump(self.examples, handle,
+                            protocol=pickle.HIGHEST_PROTOCOL)
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, item):
+        return torch.tensor(self.examples[item], dtype=torch.long)
+
+# Cacheing and storing of data/checkpoints
+
+
+def load_and_cache_examples(args, tokenizer, df_trn, df_val, evaluate=False):
+    return ConversationDataset(tokenizer, args, df_val if evaluate else df_trn)
+
+
+def set_seed(args):
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if args.n_gpu > 0:
+        torch.cuda.manual_seed_all(args.seed)
+
+
+def _sorted_checkpoints(args, checkpoint_prefix="checkpoint", use_mtime=False) -> List[str]:
+    ordering_and_checkpoint_path = []
+
+    glob_checkpoints = glob.glob(os.path.join(
+        args.output_dir, "{}-*".format(checkpoint_prefix)))
+
+    for path in glob_checkpoints:
+        if use_mtime:
+            ordering_and_checkpoint_path.append((os.path.getmtime(path), path))
+        else:
+            regex_match = re.match(
+                ".*{}-([0-9]+)".format(checkpoint_prefix), path)
+            if regex_match and regex_match.groups():
+                ordering_and_checkpoint_path.append(
+                    (int(regex_match.groups()[0]), path))
+
+    checkpoints_sorted = sorted(ordering_and_checkpoint_path)
+    checkpoints_sorted = [checkpoint[1] for checkpoint in checkpoints_sorted]
+    return checkpoints_sorted
+
+
+def _rotate_checkpoints(args, checkpoint_prefix="checkpoint", use_mtime=False) -> None:
+    if not args.save_total_limit:
+        return
+    if args.save_total_limit <= 0:
+        return
+
+    # Check if we should delete older checkpoint(s)
+    checkpoints_sorted = _sorted_checkpoints(
+        args, checkpoint_prefix, use_mtime)
+    if len(checkpoints_sorted) <= args.save_total_limit:
+        return
+
+    number_of_checkpoints_to_delete = max(
+        0, len(checkpoints_sorted) - args.save_total_limit)
+    checkpoints_to_be_deleted = checkpoints_sorted[:number_of_checkpoints_to_delete]
+    for checkpoint in checkpoints_to_be_deleted:
+        logger.info(
+            "Deleting older checkpoint [{}] due to args.save_total_limit".format(checkpoint))
+        shutil.rmtree(checkpoint)
+
+
+# Build model
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
+model = AutoModelWithLMHead.from_pretrained("microsoft/DialoGPT-small")
+
+"""
+Fine-tuning the library models for language modeling on a text file (GPT, GPT-2, BERT, RoBERTa).
+GPT and GPT-2 are fine-tuned using a causal language modeling (CLM) loss while BERT and RoBERTa are fine-tuned
+using a masked language modeling (MLM) loss.
+"""
+
+# Configs
+logger = logging.getlogger(__name__)
+
+MODEL_CONFIG_CLASSES = list(MODEL_WITH_LM_HEAD_MAPPING.keys())
+MODEL_TYPES = tuple(conf.model_type for conf in MODEL_CONFIG_CLASSES)
+
+
+# Args to allow for easy convertion of python script to notebook
+class Args():
+    def __init__(self):
+        self.output_dir = 'output-small'
+        self.model_type = 'gpt2'
+        self.model_name_or_path = 'microsoft/DialoGPT-small'
+        self.config_name = 'microsoft/DialoGPT-small'
+        self.tokenizer_name = 'microsoft/DialoGPT-small'
+        self.cache_dir = 'cached'
+        self.block_size = 512
+        self.do_train = True
+        self.do_eval = True
+        self.evaluate_during_training = False
+        self.per_gpu_train_batch_size = 4
+        self.per_gpu_eval_batch_size = 4
+        self.gradient_accumulation_steps = 1
+        self.learning_rate = 5e-5
+        self.weight_decay = 0.0
+        self.adam_epsilon = 1e-8
+        self.max_grad_norm = 1.0
+        self.num_train_epochs = 4
+        self.max_steps = -1
+        self.warmup_steps = 0
+        self.logging_steps = 1000
+        self.save_steps = 3500
+        self.save_total_limit = None
+        self.eval_all_checkpoints = False
+        self.no_cuda = False
+        self.overwrite_output_dir = True
+        self.overwrite_cache = True
+        self.should_continue = False
+        self.seed = 42
+        self.local_rank = -1
+        self.fp16 = False
+        self.fp16_opt_level = 'O1'
+
+
+args = Args()
